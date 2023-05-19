2412e97865018471f0e31abf95d25597ac24bd64
Adeyemi Joseph <75547432+Bonhomie95@users.noreply.github.com>
2023-05-19 14:16:40 -0400

    login done

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next';
+const Home: NextPage = () => {
+  return (
+    <>
+      <h1 className="text-2xl text-green-400"> Netflix Clone</h1>
+    </>
+  );
+};
+
+export default Home;
